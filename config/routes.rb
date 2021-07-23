d0a1c5b77841bdae9d6a01cbd25febed2f7328a2
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-23 14:40:57 +0000

    addgenerated user module and the migration changes for same

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
-  resources :user, only:[:register, :login, :logout]
+  resources :users, only: [:register, :login, :logout]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
